68e36c8a3a4c37ddbf0e4a3b38b2141e0a13c4ce
CongNT <congnt@investidea.tech>
2022-08-07 01:25:03 +0700

    zxc

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-// var corsOptions = {
-//   origin: "secretspage.com",
-// };
+const allowedOrigins = ['localhost:5001',
+                      'secretspage.com',
+                      'www.secretspage.com',
+                      'setup.secretspage.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 
-// app.use(cors(corsOptions));
+}));
 
 
 // parse requests of content-type - application/json
