3be9b56a7240ac8b902772284005f6112dcf80e1
CongNT <congnt@investidea.tech>
2022-08-07 00:47:44 +0700

    zxc

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const cors = require("cors");
 
 const app = express();
 
-// var corsOptions = {
-//   origin: "secretspage.com",
-// };
+var corsOptions = {
+  origin: "secretspage.com",
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // parse requests of content-type - application/json
 app.use(express.json());
 
