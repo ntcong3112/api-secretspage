063408b7b2760cfc5ecaadb550b91f5f1e6fadc6
CongNT <congnt@investidea.tech>
2022-08-06 16:04:24 +0700

    querry db

--- app/routes/turorial.routes.js
+++ app/routes/turorial.routes.js
@@ -4,7 +4,7 @@ module.exports = app => {
   var router = require("express").Router();
 
   // Create a new Tutorial
-  router.post("/", user.create);
+  router.post("/user", user.create);
 
   // Retrieve all Tutorials
   router.get("/", user.findAll);
